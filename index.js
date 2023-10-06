795f7f204f0c833da91300e47010489faa6758ab
Khrystyna Klapushchak <klapushchak.khr@gmail.com>
2023-10-06 10:26:39 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(array){
+    const resultObject = array.find(extraFunc)
+    if(resultObject===undefined) return undefined;
+    return resultObject.year
+}
+
+function extraFunc(element, index, array){
+    return (element.result==='W')
+}
